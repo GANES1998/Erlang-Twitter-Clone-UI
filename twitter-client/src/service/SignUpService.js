91cf9b822a3a7f4b48dd7e7929534d587f5690da
GANES1998 <ganes01ravi@gmail.com>
2022-12-15 20:25:34 -0500

    client services endpoints updated

--- twitter-client/src/service/SignUpService.js
+++ twitter-client/src/service/SignUpService.js
@@ -2,5 +2,5 @@ import Request from "./Request";
 
 export const signupService = async (username, password) => {
     const response = await Request.get('/signup', {params:{username, password}});
-    return response.data;
+    return response.data.data;
 }
--- twitter-client/src/service/TweetService.js
+++ twitter-client/src/service/TweetService.js
@@ -1,25 +1,25 @@
 import Request from "./Request";
 
 export const postTweet = async (userId, tweetContent) => {
-    const response = await Request.post('/tweets', {userId, tweet: tweetContent});
-    return response.data;
+    const response = await Request.get('/tweets', {params: {userId, tweet: tweetContent}});
+    return response.data.data;
 }
 
 export const getMentions = async (userId) => {
-    const response = await Request.get("/tweets/mentions", {params: {user: userId}});
-    return response.data;
+    const response = await Request.get("/getmentions", {params: {user: userId}});
+    return response.data.data;
 }
 
 export const getCurrentUserFeeds = async (userId) => {
-    const response = await Request.get("/tweets/feeds", {params: {user: userId}});
-    return response.data;
+    const response = await Request.get("/feeds", {params: {user: userId}});
+    return response.data.data;
 }
 
 export const getHashtags = async (hashtag) => {
-    const response = await Request.get("/tweets/hashtags", {params: {hashtag: hashtag}});
-    return response.data;
+    const response = await Request.get("/searchhashtags", {params: {hashtag: hashtag}});
+    return response.data.data;
 }
 
 export const retweet = async (user, tweetId) => {
-    await Request.get("/tweets/retweet", {params: {user, tweetId}});
+    await Request.get("/retweet", {params: {user, tweetId}});
 }
